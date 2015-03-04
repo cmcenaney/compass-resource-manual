da9084ca6310faae616c2ae42cf2481ca279a097
Colleen McEnaney <colleenmcenaney@gmail.com>
2015-03-03 19:17:10 -0500

    added headers and fixed columns on last sheet

--- scripts/tabletop-feed.js
+++ scripts/tabletop-feed.js
@@ -1,7 +1,8 @@
 var jqueryNoConflict = jQuery;
 
 jqueryNoConflict(document).ready(function(){
-    initializeTabletopObject('https://docs.google.com/spreadsheets/d/1kVM7vWKGSCAoLhbIMUaCsgcd0WExq5vOC1Nu2aesv5Q/pubhtml');  
+    initializeTabletopObject('https://docs.google.com/spreadsheets/d/1kVM7vWKGSCAoLhbIMUaCsgcd0WExq5vOC1Nu2aesv5Q/pubhtml'); 
+    // $('.displayname').html('Crisis & Hotline Services'); 
 });
 
 // pull data from google spreadsheet
@@ -25,23 +26,71 @@ function createTableColumns(){
     return tableColumns;
 }
 
+function createSpecialTableColumns(){
+    var specialTableColumns =   [
+        {'mDataProp': 'county', 'sTitle': 'County', 'sClass': 'left'},
+        {'mDataProp': 'organization', 'sTitle': 'Organization', 'sClass': 'left'},
+        {'mDataProp': 'city', 'sTitle': 'City', 'sClass': 'left'},
+        {'mDataProp': 'phone', 'sTitle': 'Phone Number', 'sClass': 'left'},
+        {'mDataProp': 'website', 'sTitle': 'Website', 'sClass': 'left'}
+    ];
+
+    return specialTableColumns;
+}
+
 // create the table container and object
 function writeTableWith(dataSource){
+
+
+    
     $.each(dataSource, function(key, value){
+        var thisName = nameSheets(value);
+        
+
+        value.display = thisName;
+        // $('.tab-content').prepend('<h2>'+value.display+'</h2>');
+    
+       
         var thisValue = value;
         var thisItem = key.substring(5);
 
+
         $.each(value.elements, function(key, value){
+            
+          
             itemWebsite = value.website;
             value.website = '<a href="http://'+itemWebsite+'" target="_blank">'+itemWebsite+'</a>';
             jqueryNoConflict('#demo'+thisItem).html('<table cellpadding="0" cellspacing="0" border="0" class="display table table-bordered table-striped" id="data-table-container'+thisItem+'"></table>');
 
         });
-    });
 
-    $.each(dataSource, function(key, value){
-        var thisItem = key.substring(5);
-        jqueryNoConflict('#data-table-container'+thisItem).dataTable({
+        
+        // jqueryNoConflict('#data-table-container'+thisItem).dataTable({
+        //     'sPaginationType': 'bootstrap',
+        //     'iDisplayLength': 25,
+        //     'aaData': value.elements,
+        //     'aoColumns': createTableColumns(),
+        //     'oLanguage': {
+        //         'sLengthMenu': '_MENU_ records per page'
+        //     }
+        // });
+
+        if (key =='Sheet19'){
+            $('.displayname').html(value.display);
+            jqueryNoConflict('#data-table-container'+thisItem).dataTable({
+            'sPaginationType': 'bootstrap',
+            'iDisplayLength': 25,
+            'aaData': value.elements,
+            'aoColumns': createSpecialTableColumns(),
+            'oLanguage': {
+                'sLengthMenu': '_MENU_ records per page'
+            }
+        });
+            
+        }else{
+
+         
+            jqueryNoConflict('#data-table-container'+thisItem).dataTable({
             'sPaginationType': 'bootstrap',
             'iDisplayLength': 25,
             'aaData': value.elements,
@@ -50,8 +99,38 @@ function writeTableWith(dataSource){
                 'sLengthMenu': '_MENU_ records per page'
             }
         });
-    })
+        }
+
+    });
+
+    
 };
+
+function nameSheets(value){
+    switch(value.name){
+        case 'Sheet1':
+            return 'Child Services & Resources';
+            break;
+        case 'Sheet2':
+            return 'Crisis & Hotline Services';
+            break;
+        case 'Sheet3':
+            return 'Disability';
+        default:
+            return '';
+    }
+}
+
+
+
+$('.sidebar-nav li a').click(function(){
+    $('.container-fluid h3').empty();
+   
+    var thisName = this.innerHTML;
+
+    $('.displayname').html(thisName);
+});
+
 
 //define two custom functions (asc and desc) for string sorting
 jQuery.fn.dataTableExt.oSort['string-case-asc']  = function(x,y) {
